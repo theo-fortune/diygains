9180b969131528a546355eae203fd17f002ad7b0
Theo Fortune <theofortunefunction@gmail.com>
2024-02-11 17:19:34 +0100

    nav-res

--- src/pages/LandingPage/Components/Nav/Nav.jsx
+++ src/pages/LandingPage/Components/Nav/Nav.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import { Link } from "react-router-dom";
 import { RiMenu4Fill, RiCloseLine } from "react-icons/ri";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav>
       <h2 className="logo">DiyGains</h2>
@@ -19,7 +20,30 @@ const Navbar = () => {
           <button>Sign Up</button>
           <button>Log In</button>
         </div>
-        <RiMenu4Fill />
+        <div className="nav-menu">
+          {toggleMenu ? (
+            <RiCloseLine
+              color="var(--raisin-black)"
+              onClick={() => setToggleMenu(false)}
+              className="exit"
+            />
+          ) : (
+            <RiMenu4Fill
+              color="var(--raisin-black)"
+              onClick={() => setToggleMenu(true)}
+              className="menu"
+            />
+          )}
+          {toggleMenu && (
+            <div className="nav-links_mobile scale-up-center">
+              <Link>How It Works</Link>
+              <Link>Home</Link>
+              <Link>About Us</Link>
+              <Link>Features</Link>
+              <Link>Contact</Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
