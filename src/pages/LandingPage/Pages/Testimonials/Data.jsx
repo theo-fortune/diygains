848c69964ff98af574ab072defdf4c19cf01591f
Theo Fortune <theofortunefunction@gmail.com>
2024-02-27 23:59:47 +0100

    night

---
+++ src/pages/LandingPage/Pages/Testimonials/Data.jsx
@@ -0,0 +1,73 @@
+import {
+  Quote,
+  User1,
+  User2,
+  User3,
+  User4,
+  User5,
+  User6,
+  User7,
+} from "../../../../assets";
+
+export const Data = [
+  {
+    id: 1,
+    name: "Marilyn Dorwart",
+    title: "Alphacam Investments",
+    image: User1,
+    description:
+      "DIYGAINS has truly revolutionized my crafting journey. The community support and endless project ideas have sparked my creativity, and the rewards make every project feel even more fulfilling!",
+    quote: Quote,
+  },
+  {
+    id: 2,
+    name: "Desirae Workman",
+    title: "Dy/Dx Photo Studio",
+    image: User2,
+    description:
+      "Being a part of DIYGAINS is like having a creative family. The encouragement and positive energy from fellow makers are invaluable. The platform's intuitive design and exciting challenges keep me coming back for more!",
+    quote: Quote,
+  },
+  {
+    id: 3,
+    name: "Omar Septimus",
+    title: "Rexona",
+    image: User3,
+    description:
+      "DIYGAINS is not just a platform; it's a thriving ecosystem of creativity and growth. The rewarding experiences and the sense of community make it the perfect space for anyone passionate about DIY projects.",
+    quote: Quote,
+  },
+  {
+    id: 4,
+    name: "Omar Septimus",
+    title: "Apple Inc.",
+    image: User4,
+    description:
+      "I never thought my DIY projects could lead to such meaningful connections and rewards. DIYGAINS has exceeded my expectations, providing a space where my creativity is celebrated, and my efforts are recognized.",
+    quote: Quote,
+  },
+  {
+    id: 5,
+    name: "Kaylynn Bator",
+    image: User5,
+    description:
+      "Joining DIYGAINS was the best decision for my DIY endeavors. The platform's simplicity, combined with the diverse range of projects and rewards, has made my crafting journey both enjoyable and enriching.",
+    quote: Quote,
+  },
+  {
+    id: 6,
+    name: "Desirae Rosser",
+    image: User6,
+    description:
+      "DIYGAINS has transformed my crafting routine into a fulfilling experience. The referral program is a fantastic addition – it not only brings in more like-minded individuals but also rewards us for building a stronger community.",
+    quote: Quote,
+  },
+  {
+    id: 7,
+    name: "Cooper Vetrovs",
+    image: User7,
+    description:
+      "DIYGAINS has transformed my crafting routine into a fulfilling experience. The referral program is a fantastic addition – it not only brings in more like-minded individuals but also rewards us for building a stronger community.",
+    quote: Quote,
+  },
+];
---
+++ src/pages/LandingPage/Pages/Testimonials/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <section className="testimonials">
+      <h1>What Our Users Say</h1>
+    </section>
+  );
+};
+
+export default Testimonials;
