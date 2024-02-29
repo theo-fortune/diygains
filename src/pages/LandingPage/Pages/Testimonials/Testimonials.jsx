f5af18084d7779893c12dc7ad1ab71199e17098c
Theo Fortune <theofortunefunction@gmail.com>
2024-02-29 03:13:03 +0100

    just a lil bit of css needed now

--- src/pages/LandingPage/Pages/Testimonials/Testimonials.jsx
+++ src/pages/LandingPage/Pages/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./testimonials.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -7,13 +7,54 @@ import { Pagination, Autoplay } from "swiper/modules";
 import { Data } from "./Data";
 
 const Testimonials = () => {
+  // Define breakpoints and corresponding spaceBetween values
+  const breakpoints = {
+    1024: {
+      spaceBetween: 20,
+    },
+    //* Add more breakpoints as needed
+  };
+
+  //* Function to generate responsive spaceBetween based on breakpoints
+  const getResponsiveSpace = () => {
+    const screenWidth = window.innerWidth;
+
+    //* Find the applicable breakpoint
+    const breakpoint = Object.keys(breakpoints).find(
+      (bp) => screenWidth <= parseInt(bp)
+    );
+
+    //* Return the corresponding spaceBetween value
+    return breakpoints[breakpoint]?.spaceBetween || 30; //* Default value if no breakpoint matches
+  };
+
+  const [centered, setCentered] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Update centered state based on screen width
+      setCentered(window.innerWidth > 734);
+    };
+
+    // Attach event listener on component mount
+    window.addEventListener("resize", handleResize);
+
+    // Call the handler once to initialize the state
+    handleResize();
+
+    // Detach event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array means this effect runs only on mount and unmount
+
   return (
     <section className="testimonials">
       <h1>What Our Users Say</h1>
       <Swiper
         slidesPerView={"auto"}
-        spaceBetween={30}
-        centeredSlides={true}
+        spaceBetween={getResponsiveSpace()}
+        centeredSlides={centered}
         loop={true}
         pagination={{
           clickable: true,
