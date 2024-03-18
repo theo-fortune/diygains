e6841b5bf03f9354bc8d37e429c6673eb1608394
Theo Fortune <theofortunefunction@gmail.com>
2024-03-18 13:24:35 +0100

    nav done

---
+++ src/pages/Dashboard/User/Components/Nav/Nav.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { FaBell } from "react-icons/fa";
+import { Profile } from "../../../assets";
+import { GoBellFill } from "react-icons/go";
+import "./nav.css";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  const [greeting, setGreeting] = useState("");
+  useEffect(() => {
+    const getCurrentTime = () => {
+      const currentHour = new Date().getHours();
+
+      if (currentHour >= 5 && currentHour < 12) {
+        setGreeting("Good Morning");
+      } else if (currentHour >= 12 && currentHour < 18) {
+        setGreeting("Good Afternoon");
+      } else {
+        setGreeting("Good Evening");
+      }
+    };
+    getCurrentTime();
+    const interval = setInterval(getCurrentTime, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const firstName = "Fortune";
+  const lastName = "Theo";
+  const fullName = lastName + " " + firstName;
+  const notification = 3;
+  return (
+    <div className="overview-nav">
+      <div className="overview-nav__left">
+        <h2>
+          {greeting} {firstName}
+        </h2>
+        <p>Turn Your DIY Dreams into Real Gains – Let's Create!</p>
+      </div>
+      <div className="overview-nav__right">
+        <div className="overview-nav__right-notification">
+          <div className="overview-nav__right-notification_count">
+            <p>{notification}</p>
+          </div>
+          <GoBellFill className="overview-nav__right-notification_bell" />
+        </div>
+        <Link to={"/dashboard/profile"} className="overview-nav__right-profile">
+          <div
+            className="overview-nav__right-profile_image"
+            style={{
+              backgroundImage: `url(${Profile})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+          ></div>
+          <div className="overview-nav__right-profile_name">
+            <h4>{fullName}</h4>
+            <p>Level 5 Matrix</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
--- src/pages/Dashboard/User/Pages/Overview/Overview.jsx
+++ src/pages/Dashboard/User/Pages/Overview/Overview.jsx
@@ -1,32 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./overview.css";
-import { FaBell } from "react-icons/fa";
+import Nav from "../../Components/Nav/Nav";
 const Overview = () => {
+
   const firstName = "Fortune";
   const lastName = "Theo";
   const fullName = lastName + " " + firstName;
   const notification = 3;
   return (
     <div className="overview">
-      <div className="overview-nav">
-        <div className="overview-nav__left">
-          <h2>Good Evening {firstName}</h2>
-          <p>Turn Your DIY Dreams into Real Gains – Let's Create!</p>
-        </div>
-        <div className="overview-nav__right">
-          <div className="overview-nav__right-notification">
-            <p>{notification}</p>
-            <FaBell />
-          </div>
-          <div className="overview-nav__right-profile">
-            <div className="overview-nav__right-profile_image"></div>
-            <div className="overview-nav__right-profile_name">
-              <h4>{fullName}</h4>
-              <p>Level 5 Matrix</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Nav />
     </div>
   );
 };
